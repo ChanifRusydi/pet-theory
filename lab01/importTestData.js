e1cf21efb57e1f5a9546cc4a4b1e8d8d9a33f59f
ChanifRusydi <chanif.rusydi@gmail.com>
2022-01-15 17:54:03 +0700

    add code on importTestData

--- lab01/importTestData.js
+++ lab01/importTestData.js
@@ -1,8 +1,33 @@
 const {promisify} = require('util');
 const parse       = promisify(require('csv-parse'));
 const {readFile}  = require('fs').promises;
+const {Firestore} = require('@google-cloud/firestore');
+const {Logging} = require('@google-cloud/logging');
 
+const logName = 'pet-theory-logs-importTestData';
+// Creates a Logging client
+const logging = new Logging();
+const log = logging.log(logName);
+const resource = {
+  type: 'global',
+};
 if (process.argv.length < 3) {
+  const db = new Firestore();
+  function writeToFirestore(records) {
+  const batchCommits = [];
+  let batch = db.batch();
+  records.forEach((record, i) => {
+    var docRef = db.collection('customers').doc(record.email);
+    batch.set(docRef, record);
+    if ((i + 1) % 500 === 0) {
+      console.log(`Writing record ${i + 1}`);
+      batchCommits.push(batch.commit());
+      batch = db.batch();
+    }
+  });
+  batchCommits.push(batch.commit());
+  return Promise.all(batchCommits);
+}
   console.error('Please include a path to a csv file');
   process.exit(1);
 }
@@ -18,7 +43,8 @@ async function importCsv(csvFileName) {
   const fileContents = await readFile(csvFileName, 'utf8');
   const records = await parse(fileContents, { columns: true });
   try {
-    await writeToDatabase(records);
+    // await writeToDatabase(records);
+    await writeToFirestore(records);
   }
   catch (e) {
     console.error(e);
